91918353717813baf5ee9d08d2582df24b6e5c78
Andrija-Sevaljevic <andrijasevaljevic@isu.edu>
2023-10-30 17:17:27 -0600

    m Reading example file

--- CS4416/Program.cs
+++ CS4416/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Scanner {
+    static void Main(string[] args) {
+        // Specify the path to your text file
+        string filePath = "example.txt"; // Replace with the actual file path
+
+        try
+        {
+            // Read the contents of the text file into a string
+            string fileContents = File.ReadAllText(filePath);
+
+            // Print the contents to the console (for demonstration)
+            Console.WriteLine("File Contents:");
+            Console.WriteLine(fileContents);
+
+            // You can now work with the 'fileContents' string in your program
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine($"An error occurred while reading the file: {e.Message}");
+        }
+    }
+}
