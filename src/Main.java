7a4e179cf1d2d952c15fba833b302518995a25f6
Tyler <doddtyle@isu.edu>
2023-11-06 19:41:05 -0700

    Add user prompt and file reader

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,59 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.File;
+import java.io.InputStream;
 import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.awt.*;
+import java.beans.*;
+import java.net.*;
+import java.nio.*;
 
 public class Main {
+    private static File FILE;
 
     public static void main(String[] args){
-        System.out.println("Hello World");
+        File cwd = new File("src/Main");
+        System.out.println("Program file location: " + cwd.getAbsolutePath());
+        System.out.println("Enter the relative path of the file that you would like to parse, or press enter for default.");
+
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            String userInput = bufferedReader.readLine();
+            try{
+                if(userInput.equals("")){
+                    FILE = new File("src/test.c");
+                }
+                else{
+                    FILE = new File(userInput);
+                }
+            }
+            catch(Exception e){
+                System.out.println(e.toString());
+                System.out.println("Invalid Input");
+            }
+
+            FileReader reader = new FileReader(FILE);
+            String fileContents = "";
+            boolean reading = true;
+            do{
+                int nextChar = reader.read();
+                if(nextChar == -1){ //EOF reached
+                    reading = false;
+                }
+                fileContents += (char)nextChar;
+            }while(reading);
+            System.out.println("File contents have been read");
+            System.out.print(fileContents);
+            System.out.println();
+        }
+        catch(Exception e){
+            System.out.println(e.toString());
+            e.printStackTrace();
+        }
     }
 
 }
