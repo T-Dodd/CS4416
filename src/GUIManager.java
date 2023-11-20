e588f95d326b9f0885969694562fa4b1849c5ee7
Jetison333 <Jetison333@hotmail.com>
2023-11-20 13:30:59 -0700

    filled stubs

--- src/GUIManager.java
+++ src/GUIManager.java
@@ -2,9 +2,25 @@ import java.io.*;
 /*
 * Manages the interaction with the GUI and the backend
 * */
+import java.util.Map;
 public class GUIManager {
-    public void removeFunction(String badFunc){}
-    public void addFunction(String badFunc){}
-    public void editSuggestions(String badFunc, String suggestions){}
+    public void removeFunction(String badFunc, Map<String,String> dict){
+        if (dict.containsKey(badFunc)) {
+            dict.remove(badFunc);
+        }
+    }
+
+    public void addFunction(String badFunc, Map<String, String> dict){
+        if (!dict.containsKey(badFunc)) {
+            dict.put(badFunc, "");
+        }
+    }
+
+    public void editSuggestions(String badFunc, String suggestions, Map<String, String> dict) {
+        if (dict.containsKey(badFunc)) {
+            dict.remove(badFunc);
+        }
+        dict.put(badFunc, suggestions);
+    }
 
 }
